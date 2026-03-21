fa8f27ec574a1dc89f3270a6957c02b86f2be99e
BacLuc <lucius.bachmann@gmx.ch>
2026-03-21 19:44:19 +0100

    ai_agent_devcontainer: support git worktrees

--- roles/ai_agent_devcontainer/files/start-ai-agent-devcontainer.sh
+++ roles/ai_agent_devcontainer/files/start-ai-agent-devcontainer.sh
@@ -17,4 +17,20 @@ SANITIZED_PATH=$(echo "$WORKING_DIR" | tr '/' '_' | sed 's/^_//' | sed 's/_$//')
 COMPOSE_PROJECT_NAME="ai_devcontainer_${SANITIZED_PATH}"
 export COMPOSE_PROJECT_NAME
 
+mkdir -p /tmp/noworktree
+if [[ -f $WORKSPACE_DIR/.git ]]; then
+  if grep -q "../" $WORKSPACE_DIR/.git ; then
+    WORKTREE_DIR=$(git rev-parse --git-dir)
+    WORKTREE_GIT_DIR=$(basename $(realpath $WORKTREE_DIR/../../../))
+    RELATIVE_WORKTREE_SOURCE=$(realpath --relative-to=. $WORKTREE_DIR)
+    if echo "$RELATIVE_WORKTREE_SOURCE" | grep -q "../"; then
+      export GIT_WORKTREE_SOURCE=$(realpath $WORKTREE_DIR/../../../)
+      export GIT_WORKTREE_TARGET="/workspaces/$WORKTREE_GIT_DIR"
+    fi
+    if echo "$RELATIVE_WORKTREE_SOURCE" | grep -q "../../"; then
+      export GIT_WORKTREE_TARGET="/$WORKTREE_GIT_DIR"
+    fi
+  fi
+fi
+
 devcontainer up --workspace-folder . --config "$CONFIG_DIR/devcontainer.json"
