c3fc7af673387e3584bfaf50a8fa011b660833e5
BacLuc <lucius.bachmann@clubpage.ch>
2026-05-20 17:29:19 +0200

    pyinfra: add minimal infra for unit tests

---
+++ pyinfra/operations/filesystem_test.py
@@ -0,0 +1,8 @@
+from tempfile import NamedTemporaryFile
+
+from filesystem import dirname_of  # type: ignore[import-not-found]
+
+
+def test_dirname_of() -> None:
+    with NamedTemporaryFile() as temp_file:
+        assert dirname_of(temp_file.name) == temp_file.name.rsplit("/", 1)[0]
